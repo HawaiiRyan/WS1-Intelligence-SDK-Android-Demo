10d394b5def626409e6ff8e015caf9fa2e7f2627
Aron Aperauch <aperauch@gmail.com>
2022-01-20 18:19:25 -0500

    Working

--- app/src/main/java/com/demo/intelsdk/MainActivity.java
+++ app/src/main/java/com/demo/intelsdk/MainActivity.java
@@ -18,12 +18,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // WS1 Intelligence - CN135 - aronpso OG
-        //CrittercismConfig crittercismConfig = new CrittercismConfig();
-        //crittercismConfig.setServiceMonitoringEnabled(true);
-        //crittercismConfig.setVersionCodeToBeIncludedInVersionString(true);
 
-        Crittercism.initialize(getApplicationContext(), "6d8e6cdd4fa240db966dd7aec333ea8c00555300");
+        //Crittercism.initialize(getApplicationContext(), "6d8e6cdd4fa240db966dd7aec333ea8c00555300");  // CN135 - aronpso OG
+        Crittercism.initialize(getApplicationContext(), "3af5b80b383b48af914ab178cb3f244d00555300");  // CN420 - aron OG
         Crittercism.setLoggingLevel(Crittercism.LoggingLevel.Debug);
         Crittercism.setUsername("demo1");
         Crittercism.leaveBreadcrumb("BreadcrumbSampleApplication");
